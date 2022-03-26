f4e4e6ba22c3ed136235df6ae1dff7dee9b604f9
Igor Deminski <kartoschka17gmail.com>
2022-03-26 16:10:39 +0200

    #6 end

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from . import views
 
 
@@ -7,4 +9,6 @@ urlpatterns = [
     path('<slug:slug>/<slug:post_slug>/', views.PostDetailView.as_view(), name="post_single"),
     path('<slug:slug>/', views.PostListView.as_view(), name="post_list"),
     path('', views.HomeView.as_view(), name="home"),
+    #path('',cache_page(60*15)(views.HomeView.as_view()), name="home"),
+
 ]
